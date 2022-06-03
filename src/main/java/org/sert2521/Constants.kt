ed7d83ccd145024ba353e412dcd2ba72a71b9274
kturner44 <kturner44@4j.lane.edu>
2022-06-02 17:05:18 -0700

    prune

--- src/main/java/org/sert2521/Constants.kt
+++ src/main/java/org/sert2521/Constants.kt
@@ -1,11 +1,4 @@
 package org.sert2521
 
-/*
- * The Constants file provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This file should not be used for any other purpose.
- * All String, Boolean, and numeric (Int, Long, Float, Double) constants should use
- * `const` definitions. Other constant types should use `val` definitions.
- */
-
-/** An example constant definition. */
-const val exampleMotorCanId = 1
+const val COUNTS_PER_REVOLUTION = 1440.0
+const val WHEEL_DIAMETER = 0.0007
